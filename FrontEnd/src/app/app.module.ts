5df2382435d1c534312941b575c9fdce38fa251c
AakifBen <aakifz333@gmail.com>
2022-02-07 16:44:42 -0600

    yer

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { LinechartComponent } from './components/linechart/linechart.component';
 import {ChartsModule} from "ng2-charts";
 import { HttpClientModule } from "@angular/common/http";
 import { BarchartComponent } from './components/barchart/barchart.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LinechartComponent,
-    BarchartComponent
+    BarchartComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontEnd/src/app/components/signup/signup.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class signupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  signup(userData: any) {
+
+  }
+}
