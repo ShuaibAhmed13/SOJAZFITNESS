dd5cf79577ba02c217c9ee5248d425c0b31bb8b3
AakifBen <aakifz333@gmail.com>
2022-02-20 21:40:17 -0600

    register and some more

--- FrontEnd/src/app/components/signup/signup.component.ts
+++ FrontEnd/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../services/user.services";
 
 @Component({
   selector: 'app-signup',
@@ -7,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class signupComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userService:UserService) { }
 
   ngOnInit(): void {
   }
 
   signup(userData: any) {
-
+this.userService.register({email:userData.value.email,username:userData.value.username,firstname:userData.value.firstname,lastname:userData.value.lastname,password:userData.value.password})
   }
 }
