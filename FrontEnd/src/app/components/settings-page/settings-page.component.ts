0ab556c3d0b8c2ab24664d26e2e2255ca282b1f7
jonathancampos <b2366870@ben.edu>
2022-03-03 13:35:39 -0600

    updated navbar, and workout manual

--- FrontEnd/src/app/components/settings-page/settings-page.component.ts
+++ FrontEnd/src/app/components/settings-page/settings-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ConnectionService} from "../../connection.service";
 
 @Component({
   selector: 'app-settings-page',
@@ -6,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings-page.component.scss']
 })
 export class SettingsPageComponent implements OnInit {
+  panelOpenState = false;
+  contactForm: FormGroup;
+  disabledSubmitButton: boolean = true;
+  optionsSelect: Array<any> | undefined;
 
-  constructor() { }
+  @HostListener('input') oninput() {
+
+    if (this.contactForm.valid) {
+      this.disabledSubmitButton = false;
+    }
+  }
+
+  constructor(private fb: FormBuilder, private connectionService: ConnectionService) {
+
+    this.contactForm = fb.group({
+      'contactFormName': ['', Validators.required],
+      'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
+      'contactFormSubjects': ['', Validators.required],
+      'contactFormMessage': ['', Validators.required],
+      'contactFormCopy': [''],
+    });
+  }
+
+  onSubmit() {
+    this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
+      alert('Your message has been sent.');
+      this.contactForm.reset();
+      this.disabledSubmitButton = true;
+    }, error => {
+      console.log('Error', error);
+    });
+  }
 
   ngOnInit(): void {
   }
