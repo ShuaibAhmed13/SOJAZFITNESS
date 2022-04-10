e29a4f8413bd3fc08b239d2489fdb31938b311df
Shuaib Ahmed <ahmedshuaib34@yahoo.com>
2022-04-09 21:31:39 -0500

    Completed most of, if not all of, the backend for userexercise (user exercise diary).

--- SOJAZBackend/src/main/java/edu/ben/SOJAZBackend/controller/UserExerciseResource.java
+++ SOJAZBackend/src/main/java/edu/ben/SOJAZBackend/controller/UserExerciseResource.java
@@ -9,6 +9,8 @@ import edu.ben.SOJAZBackend.service.UserFoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/userexercise")
 public class UserExerciseResource {
@@ -20,13 +22,45 @@ public class UserExerciseResource {
         this.userExerciseService = userExerciseService;
     }
 
-    @PostMapping("/addtocardiotexercisediary/{exerciseId}/{userId}/{timeOfDay}/{noOfMinutes}")
-    public void addToCardioExerciseDiary(@PathVariable Long exerciseId, @PathVariable Long userId, @PathVariable String timeOfDay, @PathVariable double noOfMinutes) {
-        userExerciseService.addCardioExerciseToDiary(exerciseId, userId, timeOfDay, noOfMinutes);
+    @PostMapping("/addtocardioexercisediary/{exerciseId}/{userId}")
+    public void addToCardioExerciseDiary(@PathVariable Long exerciseId, @PathVariable Long userId, @RequestBody CardioInfo cardioInfo) {
+        userExerciseService.addCardioExerciseToDiary(exerciseId, userId, cardioInfo.timeOfDay, cardioInfo.noOfMinutes);
     }
 
     @PostMapping("/addtoweightexercisediary/{exerciseId}/{userId}")
-    public void addToWeightExerciseDiary(@RequestBody UserExerciseDTO userExerciseDTO, @PathVariable Long exerciseId, @PathVariable Long userId) {
-        userExerciseService.addWeightExerciseToDiary(exerciseId, userId, userExerciseDTO.getTimeOfDay(), userExerciseDTO.getSets(), userExerciseDTO.getReps());
+    public void addToWeightExerciseDiary(@RequestBody WeightInfo weightInfo, @PathVariable Long exerciseId, @PathVariable Long userId) {
+        userExerciseService.addWeightExerciseToDiary(exerciseId, userId, weightInfo.timeOfDay, weightInfo.sets, weightInfo.reps);
+    }
+
+    @GetMapping("/getusersexercises/{user_id}")
+    public List<UserExercise> getUsersExercises(@PathVariable Long user_id) {
+        return userExerciseService.getUserExercises(user_id);
+    }
+    @PutMapping("/edituserexercise/{userexercise_id}")
+    public void editUserExercise(@PathVariable Long userexercise_id, @RequestBody UserExercise userExercise) {
+        userExerciseService.editUserExercise(userexercise_id, userExercise);
+    }
+    @DeleteMapping("/deleteuserexercise/{userexercise_id}")
+    public void deleteUserExercise(@PathVariable Long userexercise_id) {
+        userExerciseService.deleteUserExercise(userexercise_id);
+    }
+}
+class CardioInfo {
+    String timeOfDay;
+    double noOfMinutes;
+    public CardioInfo(String timeOfDay, double noOfMinutes) {
+        this.timeOfDay = timeOfDay;
+        this.noOfMinutes = noOfMinutes;
+    }
+}
+
+class WeightInfo {
+    String timeOfDay;
+    int sets;
+    int reps;
+    public WeightInfo(String timeOfDay, int sets, int reps) {
+        this.timeOfDay = timeOfDay;
+        this.sets = sets;
+        this.reps = reps;
     }
 }
