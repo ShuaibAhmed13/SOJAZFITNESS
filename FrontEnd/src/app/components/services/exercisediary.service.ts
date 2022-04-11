6aa6ee7a7aaca9659a87236400ea1297cdc37df8
BUOmerBebo <b2477332@ben.edu>
2022-04-10 19:04:25 -0500

    finishing touches on diary

--- FrontEnd/src/app/components/services/exercisediary.service.ts
+++ FrontEnd/src/app/components/services/exercisediary.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {Observable} from "rxjs";
 import {Fooddiary} from "../interfaces/fooddiary";
+import {Exercisediary} from "../interfaces/Exercisediary";
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,21 @@ export class ExercisediaryService {
 
   constructor(private httpClient: HttpClient, private router: Router) { }
 
-  addExercise(time: string, exerciseId: number, userId:number): Observable<string> {
-    const body = {time: time}
-    return this.httpClient.post<string>('api/userexercise/addtoexercisediary/' + exerciseId + "/" + userId, body);
+  addCardioExercise(timeOfDay: string, noOfMinutes: number, exerciseId: number, userId:number): Observable<string> {
+    const body = {timeOfDay: timeOfDay, noOfMinutes: noOfMinutes}
+    return this.httpClient.post<string>('api/userexercise/addtocardioexercisediary/' + exerciseId + "/" + userId, body);
   }
 
+  addWeightExercise(timeOfDay: string, sets: number, reps: number, exerciseId: number, userId:number): Observable<string> {
+    const body = {timeOfDay: timeOfDay, sets: sets, reps: reps}
+    return this.httpClient.post<string>('api/userexercise/addtoweightexercisediary/' + exerciseId + "/" + userId, body);
+  }
+
+  getUsersExerciseDiary(userId: number): Observable<Exercisediary[]> {
+    return this.httpClient.get<Exercisediary[]>('api/userexercise/getusersexercises/' + userId);
+  }
+
+  deleteFromExerciseDiary(entryId: number) {
+    return this.httpClient.delete('api/userexercise/deleteuserexercise/' + entryId);
+  }
 }
